837c8eed1c6eafafcea82253caf62c6abf9a120f
stefaniazocco <stefaniazocco93@gmail.com>
2022-07-30 21:28:41 +0200

    add header

--- product-script.js
+++ product-script.js
@@ -26,3 +26,25 @@ function setSelectTitle(e) {
 };
 
 
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+/*function myFunction() {
+	document.getElementById("dropdown").classList.toggle("show");
+  }*/
+
+
+
+// Close the dropdown if the user clicks outside of it
+/*window.onclick = function(event) {
+	if (!event.target.matches('.button')) {
+	  var dropdowns = document.getElementsById('dropdown');
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }*/
